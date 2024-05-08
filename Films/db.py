f5a572e80d79efa4b8d08421702340f846b91d10
adriancolman <adriangamarra093@gmail.com>
2024-05-08 10:32:38 -0300

    Hacer una pagina sencilla con una lista de elementos que mustra canciones y peliculas

--- Films/db.py
+++ Films/db.py
@@ -1,16 +1,27 @@
 import sqlite3
+import os
 
 import click
 from flask import current_app, g
 
 
+db_folder = current_app.instance_path;
+db_name = "peliculas.sqlite"
+db_file = os.path.join(db_folder,db_name)
+db_sql_file = "datos.sql"  
+
+def dict_factory(cursor, row):
+ """Arma un diccionario con los valores de la fila."""
+ fields = [column[0] for column in cursor.description]
+ return {key: value for key, value in zip(fields, row)}
+
 def get_db():
     if 'db' not in g:
         g.db = sqlite3.connect(
-            current_app.config['DATABASE'],
+            db_file,
             detect_types=sqlite3.PARSE_DECLTYPES
         )
-        g.db.row_factory = sqlite3.Row
+        g.db.row_factory = dict_factory
 
     return g.db
 
@@ -22,9 +33,13 @@ def close_db(e=None):
         db.close()
 
 def init_db():
+    try:
+        os.makedirs(db_folder)
+    except OSError:
+        pass
     db = get_db()
 
-    with current_app.open_resource('schema.sql') as f:
+    with current_app.open_resource(db_sql_file) as f:
         db.executescript(f.read().decode('utf8'))
 
 
@@ -37,9 +52,5 @@ def init_db_command():
 
 def init_app(app):
     app.teardown_appcontext(close_db)
-    app.cli.add_command(init_db_command)
+    app.cli.add_command(init_db_command) 
 
-def dict_factory(cursor, row):
- """Arma un diccionario con los valores de la fila."""
- fields = [column[0] for column in cursor.description]
- return {key: value for key, value in zip(fields, row)}
